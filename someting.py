1fee85ee5a7126c0f686c2e425f5a10ee51ca03d
copernicmh <mhcopernic@gmail.com>
2026-08-21 01:07:20 -0400

    n

--- someting.py
+++ someting.py
@@ -8,9 +8,12 @@ pandl_percentage = pandl * 100
 ''' let's say we want to create a machine learning scheme to converge upon appropriate values
 within some boundary a and b. the boundary are the support and resistance bands for a given set of candles
 over a time period s. firstly, to measure momentum of given regions under a curve we will use the integral
-these curves accentuate themselves at the maxima and min. secondly, there exist another underlying parabola,
+these curves accentuate themselves at the maxima and min. 
+
+secondly, there exist another underlying parabola,
 we can define the collision of one piecewise step function to another as the pinnacle, where the parabola connects
 at a point X and we know the parabola to have an equation
+
 this pinnacle can be analyzed to converge on future pinnacle magnitudes and frequencies.
 once these values are realized one can predict the velocity, distance, size and geometric application
 of the underlying asset.
