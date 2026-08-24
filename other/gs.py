4feedb5a6caff37eb9e7d23023edd201bee543f9
copernicmh <mhcopernic@gmail.com>
2026-08-24 17:07:50 -0400

    test

---
+++ other/gs.py
@@ -0,0 +1,32 @@
+# variables
+
+# variables are data type stored by the programmer to hold some value for future use
+
+
+# in python data types are not defined
+
+name = 50
+print(name)
+
+name2 = 100
+print(name2)
+
+
+# math
+print(8-1)
+print(2**3) # exponentiation
+print(8//4)
+print(10.222/2)
+print((2+4)**2)
+print(not True)
+print(2>True or 0 != False)
+
+
+# control flow
+
+if name2 < name:
+    print(name, 'is bigger than', name2)
+elif name2 > name:
+    print(name2, 'is bigger than', name)
+elif name2 == name2:
+    print(name2, 'is equal to', name)
